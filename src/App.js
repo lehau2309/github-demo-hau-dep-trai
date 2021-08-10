af64a29c95a0d17e3480fae31231c127fe7c4a5c
DESKTOP-DIQPP5F\hoangthanh <lehoangnds@gmail.com>
2021-08-10 10:17:12 +0700

    add index.html file

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
+
 import './App.css';
+import BannerComponent from './components/banner/banner.component';
+import FloatingComponent from './components/floating/floating.component';
+import FooterComponent from './components/footer/footer.component';
+import HeaderComponent from './components/header/header.component';
+import LastComponent from './components/last/last.component';
+import MenuComponent from './components/menu/menu.component';
+import OthersComponent from './components/Others/others.component';
+import SellingComponent from './components/selling/selling.component';
+import TypesComponent from './components/types/types.compontent';
+import WorkComponent from './components/work/work.component';
+
 
 function App() {
+ 
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <HeaderComponent />
+      <MenuComponent />
+      <BannerComponent />
+      <SellingComponent />
+      <TypesComponent />
+      <OthersComponent />
+      <WorkComponent />
+      <FloatingComponent />
+      <FooterComponent />
+      <LastComponent />
     </div>
   );
 }
-
 export default App;
